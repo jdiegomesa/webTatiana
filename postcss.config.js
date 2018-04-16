ff15b0d00e90b41a067dd308fc2e5faddc26756a
Juan Diego Mesa <juand.mesa@hotmail.com>
2018-04-15 22:52:17 -0500

    Primera version

---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+module.exports={
+        plugins:[
+            require('postcss-import'),
+            require('postcss-mixins'),
+            require('postcss-nested'),
+            require('postcss-cssnext'),
+            require('css-mqpacker'),
+            require('csswring')
+        ]
+}
